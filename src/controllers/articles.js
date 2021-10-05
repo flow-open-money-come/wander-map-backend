ce682600b4c10684d709ce2609f13c1fb72aa9f5
Torai Lin <torailin55@gmail.com>
2021-10-05 17:21:05 +0800

    [userAuth] article api authentication & input validation

--- src/controllers/articles.js
+++ src/controllers/articles.js
@@ -1,16 +1,43 @@
-const { INVALID_INPUT } = require('../constants/errors')
-
+const { INVALID_INPUT, FORBIDDEN_ACTION } = require('../constants/errors')
 const articleModel = require('../models/articles')
+const { getPermissionLevel } = require('../utils')
+
+function checkArticlePermission(res, tokenPayload, articleId, cb) {
+  articleModel.getAuthorId(articleId, (err, result) => {
+    if (err) return cb(err)
+
+    const authorId = result[0].author_id
+    if (getPermissionLevel(tokenPayload, authorId) < 2) return res.status(403).json(FORBIDDEN_ACTION)
+    cb(null)
+  })
+}
 
 const articleController = {
   addArticle: (req, res, next) => {
-    const article = req.body
-    const articleArr = Object.values(article)
-    for (let i = 0; i < articleArr.length; i++) {
-      if (articleArr[i] === '' && i !== 4 && i !== 5) {
-        return res.status(403).json(INVALID_INPUT)
-      }
+    const { tokenPayload } = res.locals
+    if (getPermissionLevel(tokenPayload, tokenPayload.user_id) < 2) return res.status(403).json(FORBIDDEN_ACTION)
+
+    const { title, content, location, coordinate, altitude, length, departure_time, end_time, time_spent, cover_picture_url, gpx_url } = req.body
+    const author_id = tokenPayload.user_id
+    const article = {
+      author_id,
+      title,
+      content,
+      location,
+      coordinate,
+      altitude,
+      length,
+      departure_time,
+      end_time,
+      time_spent,
+      cover_picture_url,
+      gpx_url,
     }
+
+    for (column in article) {
+      if (!article[column] && article[column] !== 0) delete article[column]
+    }
+
     articleModel.add(article, (err, results) => {
       if (err) return next(err)
       res.json({
@@ -57,16 +84,40 @@ const articleController = {
 
   updateArticle: (req, res, next) => {
     const { id } = req.params
-    const article = req.body
+    const { tokenPayload } = res.locals
+    const { title, content, location, coordinate, altitude, length, departure_time, end_time, time_spent, cover_picture_url, gpx_url, is_deleted } = req.body
+    const article = {
+      title,
+      content,
+      location,
+      coordinate,
+      altitude,
+      length,
+      departure_time,
+      end_time,
+      time_spent,
+      cover_picture_url,
+      gpx_url,
+      is_deleted,
+    }
 
-    articleModel.update(id, article, (err, results) => {
+    for (column in article) {
+      if (!article[column] && article[column] !== 0) delete article[column]
+    }
+
+    checkArticlePermission(res, tokenPayload, id, (err) => {
       if (err) return next(err)
-      articleModel.findById(id, (err, results) => {
+
+      articleModel.update(id, article, (err, results) => {
         if (err) return next(err)
-        res.json({
-          success: true,
-          message: 'OK',
-          data: results,
+
+        articleModel.findById(id, (err, results) => {
+          if (err) return next(err)
+          res.json({
+            success: true,
+            message: 'OK',
+            data: results,
+          })
         })
       })
     })
@@ -74,12 +125,18 @@ const articleController = {
 
   deleteArticle: (req, res, next) => {
     const { id } = req.params
-    articleModel.delete(id, (err, results) => {
+    const { tokenPayload } = res.locals
+
+    checkArticlePermission(res, tokenPayload, id, (err) => {
       if (err) return next(err)
-      res.json({
-        success: true,
-        message: 'OK',
-        data: results,
+
+      articleModel.delete(id, (err, results) => {
+        if (err) return next(err)
+        res.json({
+          success: true,
+          message: 'OK',
+          data: results,
+        })
       })
     })
   },
--- src/routers/api/articles.js
+++ src/routers/api/articles.js
@@ -1,13 +1,15 @@
+const auth = require('../../middlewares/auth')
 const articleController = require('../../controllers/articles')
 const articleRouter = require('express').Router()
 const { PATH_ERROR } = require('../../constants/errors')
+const { articleValidator } = require('../../middlewares/validators')
 
-articleRouter.post('/', articleController.addArticle)
+articleRouter.post('/', auth, articleValidator, articleController.addArticle)
 articleRouter.get('/', articleController.getArticles)
 articleRouter.get('/hot', articleController.getHotArticles)
 articleRouter.get('/:id', articleController.getArticle)
-articleRouter.patch('/:id', articleController.updateArticle)
-articleRouter.delete('/:id', articleController.deleteArticle)
+articleRouter.patch('/:id', auth, articleValidator, articleController.updateArticle)
+articleRouter.delete('/:id', auth, articleController.deleteArticle)
 articleRouter.get('/:id/messages', articleController.getMessages)
 
 articleRouter.all('*', (req, res) => res.status(404).json(PATH_ERROR))
